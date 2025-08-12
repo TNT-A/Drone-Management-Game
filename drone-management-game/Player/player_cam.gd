85ea2ffa8ae1b2ad03c1fc0ef35c3eda3cad4779
TNT-A <andrejr.snellings@gmail.com>
2025-08-12 12:59:32 -0400

    Made the player
    I made a simple player that moves around with WASD, and a component that will allow any character body 2D node also move with WASD. I created an adaptive camera that follows slightly ahead of the player so they can see what's ahead of them better.

---
+++ drone-management-game/Player/player_cam.gd
@@ -0,0 +1,24 @@
+extends Camera2D
+
+var follow_dist : float = 50.0
+var following : bool = true
+
+func _physics_process(delta: float) -> void:
+	if following:
+		follow()
+
+func follow():
+	var dir = get_direction()
+	position = position.lerp(dir * follow_dist, 0.04)
+
+func get_direction():
+	var input : Vector2 = Vector2()
+	if Input.is_action_pressed("Move_Up"):
+		input.y -=1
+	if Input.is_action_pressed("Move_Down"):
+		input.y +=1
+	if Input.is_action_pressed("Move_Left"):
+		input.x -=1
+	if Input.is_action_pressed("Move_Right"):
+		input.x +=1
+	return input
---
+++ drone-management-game/wasd_move.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var host : CharacterBody2D
+@export var speed : int = 200
+
+var acceleration : float = 0.2
+var accept_input : bool = true
+
+func _physics_process(delta: float) -> void:
+	move()
+
+func get_direction():
+	var input : Vector2 = Vector2()
+	if Input.is_action_pressed("Move_Up"):
+		input.y -=1
+	if Input.is_action_pressed("Move_Down"):
+		input.y +=1
+	if Input.is_action_pressed("Move_Left"):
+		input.x -=1
+	if Input.is_action_pressed("Move_Right"):
+		input.x +=1
+	return input
+
+func move():
+	var dir = get_direction()
+	host.velocity = host.velocity.lerp(dir * speed, acceleration)
+	if accept_input:
+		host.move_and_slide()
